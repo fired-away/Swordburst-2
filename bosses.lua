68b02587628b3836d0fdb46238d18fd50eca93a6
tyler <81393840+fired-away@users.noreply.github.com>
2022-06-08 17:38:24 -0400

    Create bosses.lua

---
+++ bosses.lua
@@ -0,0 +1,55 @@
+local Bosses = {
+    [542351431] = {
+        "Dire Wolf";
+        "Rahjin the Thief King";
+    };
+
+    [548231754] = {
+        "Pearl Guardian";
+        "Gorrock the Grove Protector";
+        "Borik the BeeKeeper";
+    };
+
+    [555980327] = {
+        "Qerach The Forgotten Golem";
+        "Alpha Icewhal";
+        "Ra'thae the Ice King";
+    };
+
+    [572487908] = {
+        "Rotling";
+        "Irath the Lion";
+    };
+
+    [580239979] = {
+        "Fire Scorpion";
+        "Sa'jun the Centurian Chieftain";
+    };
+    
+    [582198062] = {
+        "Frogazoid";
+        "Smashroom";
+    };
+
+    [548878321] = {
+        "Hippogriff";
+        "Formaug the Jungle Giant";
+    };
+
+    [573267292] = {
+        "Gargoyle Reaper";
+        "Polyserpant";
+        "Mortis the Flaming Sear";
+    };
+
+    [2659143505] = {
+        "Baal; The Tormentor";
+        "Grim the Overseer";
+    };
+
+    [5287433115] = {
+        "Da";
+        "Ra";
+        "Ka";
+    };
+}
