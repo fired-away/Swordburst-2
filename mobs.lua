58c05dfcd4bbf3a591c9037ee407d78a8ecc6b42
tyler <81393840+fired-away@users.noreply.github.com>
2022-06-08 17:36:59 -0400

    Create mobs.lua

---
+++ mobs.lua
@@ -0,0 +1,102 @@
+local Mobs = {
+    [542351431] = {
+        "Frenzy Boar";
+        "Wolf";
+        "Hermit Crab";
+        "Bear";
+        "Ruin Knight";
+        "Draconite";
+        "Ruin Kobold Knight";
+    };
+
+    [548231754] = {
+        "Leaf Beetle";
+        "Leaf Ogre";
+        "Leafray";
+        "Pearl Keeper";
+        "Bushback Tortoise";
+        "Wasp";
+    };
+
+    [555980327] = {
+        "Snowgre";
+        "Angry Snowman";
+        "Icewhal";
+        "Ice Elemental";
+        "Snowhorse";
+        "Ice Walker";
+    };
+
+    [572487908] = {
+        "Wattlechin Crocodile";
+        "Birchman";
+        "Treehorse";
+        "Treeray";
+        "Boneling";
+        "Bamboo Spiderling";
+        "Bamboo Spider";
+        "Dungeon Dweller";
+        "Lion Protector";
+    };
+
+    [580239979] = {
+        "Girdled Lizard";
+        "Angry Cactus";
+        "Desert Vulture";
+        "Sand Scorpion";
+        "Giant Centipede";
+        "Green Patrolman";
+        "Centaurian Defender";
+        "Patrolman Elite";
+    };
+
+    [582198062] = {
+        "Jelly Wisp";
+        "Firefly";
+        "Shroom Back Clam";
+        "Gloom Shroom";
+        "Horned Sailfin Iguana";
+        "Blightmouth";
+        "Snapper"
+    };
+    
+    [548878321] = {
+        "Giant Praying Mantis";
+        "Petal Knight";
+        "Leaf Rhino";
+        "Sky Raven";
+        "Wingless Hippogriff";
+        "Forest Wanderer";
+        "Dungeon Crusador"
+    };
+
+    [573267292] = {
+        "Batting Eye";
+        "Lingerer";
+        "Fishrock Spider";
+        "Reptasaurus";
+        "Ent";
+        "Undead Warrior";
+        "Enraged Lingerer";
+        "Undead Berserker"
+    };
+
+    [2659143505] = {
+        "Winged Minion";
+        "Clay Giant";
+        "Wendigo";
+        "Grunt";
+        "Guard Hound";
+        "Minion";
+        "Shady Villager";
+        "Undead Servant";
+    };
+
+    [5287433115] = {
+        "Reaper";
+        "Soul Eater";
+        "Shadow Figure";
+        "???????";
+        "Command Falcon";
+    };
+}
